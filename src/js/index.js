9bd9b9095337b2986671b29d7f1a86fb9061dd9b
AndersonGodoy1507 <134303770+AndersonGodoy1507@users.noreply.github.com>
2024-02-01 10:45:24 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+
+
+const botao = document.querySelector(".btn-plataforma");
+
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+//Passo 3 - pegar o clique do usuario no js
+
+botao.addEventListener("click", () => {
+   
+   
+    elementoPlataformas.classList.toggle("ativo");
+
+
+
+}); 
+
+
+
+
+
+
+
+
+
